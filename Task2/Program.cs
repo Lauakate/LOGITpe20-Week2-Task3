cf6bd3c25a5863cd1c8c0080cd2ca78ba4348061
Opilane TTHK <opilane@tthk.ee>
2020-09-08 14:05:11 +0300

    Task2-3

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("When were you born?");
+          
+            string Userinput;
+            Userinput = Console.ReadLine();
+            int Year;
+            Year = Int32.Parse(Userinput);
+            int Age = 2020 - Year;
+            if (Age < 18)
+            {
+                Console.WriteLine($"you are too young to get a drivers license.");
+
+
+            }
+            else if (Age > 18)
+            {
+                Console.WriteLine("you are old enough to get a driver license.");
+                    }
+
+
+
+
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1;
+            int Number2;
+            string UserInput;
+            string UserInput2;
+
+            Console.WriteLine("Choose number one");
+            UserInput = Console.ReadLine();
+            Number1 = Int32.Parse(UserInput);
+            Console.WriteLine("Choose number two");
+            UserInput2 = Console.ReadLine();
+            Number2 = Int32.Parse(UserInput2);
+
+            if (Number1 > Number2)
+            {
+                Console.WriteLine($"{Number1} is bigger than {Number2}");
+            }
+            else if (Number1 < Number2)
+            {
+                Console.WriteLine($"{Number1} is smaller than {Number2}");
+            }
+            else
+            {
+                Console.WriteLine($"{Number1} is equal to {Number2}");
+            }
+                    
+
+        }
+    }
+}
