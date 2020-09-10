e6738c07c5bcdb4efe5fc08ff4d10c15af0a6480
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:14:47 +0300

    IfStatements

---
+++ Task4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that checks whether a given number is negative or positive
+
+            int Number;
+            string UserInput;
+
+            Console.WriteLine("Insert a negative or positive number.");
+            UserInput = Console.ReadLine();
+            Number = Int32.Parse(UserInput);
+
+            if (Number > 0)
+            {
+                Console.WriteLine("That number is positive.");
+
+            }
+            else if (Number < 0)
+            {
+                Console.WriteLine("That number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("That number is equal to 0");
+            }
+
+                
+            
+               
+
+           
+            
+         
+                    
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+
+            Console.WriteLine("What is your favorite animal?)");
+            UserInput = Console.ReadLine();
+
+            if (UserInput == "dog")
+            {
+                Console.WriteLine("People who love dogs are partygoers.");
+            }
+            else if (UserInput == "cat")
+            {
+                Console.WriteLine("People who love cats are homebirds");
+            }
+            else
+            {
+                Console.WriteLine($"You are a {UserInput} lover.");
+            }
+
+
+        }
+    }
+}
